6949e28b2bdda1c06c90a99c2332c0a80020e651
IFX-Anusha <Anusha.TR@infineon.com>
2025-10-07 16:56:40 +0530

    ./: Refactor code.
    Signed-off-by: IFX-Anusha <Anusha.TR@infineon.com>

--- src/xensiv_pas_gas_r290-ino.hpp
+++ src/xensiv_pas_gas_r290-ino.hpp
@@ -31,9 +31,9 @@ class XENSIV_PAS_GASR290Ino : public XENSIV_PAS_GASIno
         /******************************** R290 specific functions *****************************/
         Error_t getDeviceID     (uint8_t & devID);
         Error_t getAlarmConfig  (bool & activeHigh);
-        Error_t setABOCPrefill   (uint8_t hours);
         Error_t getABOCCycle    (uint8_t & days);
         Error_t setABOCCycle    (uint8_t days);
+        Error_t setABOCPrefill   (uint8_t hours);
         Error_t getDenoiseConfig(uint8_t & smoothing_factor);
         Error_t setDenoiseConfig(uint8_t smoothing_factor);
         Error_t selfTestErrorClear(bool clear);
