54941469da131c0f0ca9dd4a4f0a8e0012de96e7
zhanglinjing <Linjing.Zhang@infineon.com>
2025-11-03 17:46:29 +0100

    test/: Initialization lib unit test.
    Signed-off-by: zhanglinjing <Linjing.Zhang@infineon.com>

---
+++ tests/lib-unit-tests/arduino/test_arduino_includes.hpp
@@ -0,0 +1,13 @@
+#ifndef TEST_INCLUDES_HPP
+#define TEST_INCLUDES_HPP
+
+
+// Arduino includes
+#include <Arduino.h>
+
+// project cpp includes
+
+// test includes
+#include "test_includes.h"
+
+#endif // TEST_INCLUDES_HPP
