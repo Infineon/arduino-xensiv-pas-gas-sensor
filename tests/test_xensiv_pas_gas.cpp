f26122c9be55aa7dda8380c5d20677ac5d124be1
IFX-Anusha <Anusha.TR@infineon.com>
2025-11-20 17:11:31 +0530

    tests: Added Arduino API tests.
    Signed-off-by: IFX-Anusha <Anusha.TR@infineon.com>

--- tests/test_xensiv_pas_gas.cpp
+++ tests/test_xensiv_pas_gas.cpp
@@ -1,150 +1,208 @@
-#include "corelib/xensiv_pas_gas_regs.h"
 #include "test_arduino_includes.hpp"
 #include "xensiv_pas_gas_ino.hpp"
+#include "xensiv_pas_gas_regs.h"
 
 TEST_GROUP(XENSIV_PAS_GAS);
 static XENSIV_PAS_GASIno *gasSensor = nullptr;
-static Error_t err;
-static float testGasConc = 400.0f;                       // Example value for gas concentration
-static uint8_t testRegAddr = XENSIV_PAS_GAS_REG_PROD_ID; // Example register
+enum SensorInterfaceType
+{
+    SENSOR_IF_I2C,
+    SENSOR_IF_UART
+};
+static uint8_t testRegAddr = XENSIV_PAS_GAS_REG_CALIB_REF_H; // Writable register for tests
+
+// Default settings
+GasType_t currentSensorType = SENSOR_CO2;
+SensorInterfaceType currentInterfaceType = SENSOR_IF_I2C;
 
 TEST_SETUP(XENSIV_PAS_GAS)
 {
-    GasType_t sensorType = SENSOR_CO2;
-    gasSensor = new XENSIV_PAS_GASIno(sensorType, &Wire);
-    Wire.begin();
+    if (currentInterfaceType == SENSOR_IF_I2C)
+    {
+        Wire.begin();
+        gasSensor = new XENSIV_PAS_GASIno(currentSensorType, &Wire);
+    }
+    else
+    {
+        Serial.begin(9600);
+        gasSensor = new XENSIV_PAS_GASIno(currentSensorType, &Serial);
+    }
+    Error_t err = gasSensor->begin();
+    TEST_ASSERT_EQUAL(0, err);
 }
+
 TEST_TEAR_DOWN(XENSIV_PAS_GAS)
 {
+    Error_t err = gasSensor->end();
+    TEST_ASSERT_EQUAL(0, err);
     delete gasSensor;
     gasSensor = nullptr;
+    if (currentInterfaceType == SENSOR_IF_I2C)
+    {
+        Wire.end();
+    }
+    else
+    {
+        Serial.end();
+    }
+    delay(500);
+}
+
+void xensiv_pas_gas_suiteSetup()
+{
+    Error_t err = gasSensor->begin();
+    TEST_ASSERT_EQUAL(0, err);
+}
+
+void xensiv_pas_gas_suiteTearDown()
+{
+    Error_t err = gasSensor->end();
+    TEST_ASSERT_EQUAL(0, err);
 }
 
 TEST_IFX(XENSIV_PAS_GAS, TestSetPressRef)
 {
     uint16_t testValue = 1013;
-    gasSensor->setPressRef(testValue);
+    Error_t err = gasSensor->setPressRef(testValue);
+    TEST_ASSERT_EQUAL(0, err);
     uint8_t regData[2];
     gasSensor->getRegister(XENSIV_PAS_GAS_REG_PRESS_REF_H, regData, 1);
     uint16_t regValue = regData[0] << 8;
     gasSensor->getRegister(XENSIV_PAS_GAS_REG_PRESS_REF_L, regData + 1, 1);
     regValue |= regData[1];
     TEST_ASSERT_EQUAL(testValue, regValue);
-
-    TEST_IFX(XENSIV_PAS_GAS, TestSetABOC)
-    {
-        ABOC_t aboc = {0};
-        int16_t abocRef = 400;
-        gasSensor->setABOC(aboc, abocRef);
-        uint8_t regData[2];
-        gasSensor->getRegister(XENSIV_PAS_GAS_REG_ABOC_REF_H, regData, 1);
-        uint16_t regValue = regData[0] << 8;
-        gasSensor->getRegister(XENSIV_PAS_GAS_REG_ABOC_REF_L, regData + 1, 1);
-        regValue |= regData[1];
-        TEST_ASSERT_EQUAL(abocRef, regValue);
-    }
-
-    TEST_IFX(XENSIV_PAS_GAS, TestPerformForcedCompensation)
-    {
-        uint16_t testValue = 400;
-        gasSensor->performForcedCompensation(testValue);
-        uint8_t regData[2];
-        gasSensor->getRegister(XENSIV_PAS_GAS_REG_GAS_REF_H, regData, 1);
-        uint16_t regValue = regData[0] << 8;
-        gasSensor->getRegister(XENSIV_PAS_GAS_REG_GAS_REF_L, regData + 1, 1);
-        regValue |= regData[1];
-        TEST_ASSERT_EQUAL(testValue, regValue);
-    }
-
-    TEST_IFX(XENSIV_PAS_GAS, TestClearForcedCompensation)
-    {
-        gasSensor->clearForcedCompensation();
-        uint8_t regData[2];
-        gasSensor->getRegister(XENSIV_PAS_GAS_REG_GAS_REF_H, regData, 1);
-        uint16_t regValue = regData[0] << 8;
-        gasSensor->getRegister(XENSIV_PAS_GAS_REG_GAS_REF_L, regData + 1, 1);
-        regValue |= regData[1];
-        TEST_ASSERT_EQUAL(0, regValue);
-    }
-
-    TEST_IFX(XENSIV_PAS_GAS, TestGetGasConcentration)
-    {
-        float value = 0.0f;
-        gasSensor->getGasConcentration(value);
-        uint8_t regData[2];
-        gasSensor->getRegister(XENSIV_PAS_GAS_REG_GASCONC_H, regData, 1);
-        uint16_t regValue = regData[0] << 8;
-        gasSensor->getRegister(XENSIV_PAS_GAS_REG_GASCONC_L, regData + 1, 1);
-        regValue |= regData[1];
-        float regConc = (float)regValue; // Conversion may be needed
-        TEST_ASSERT_EQUAL(regConc, value);
-    }
-
-    TEST_IFX(XENSIV_PAS_GAS, TestGetDiagnosis)
-    {
-        Diag_t diag = 0;
-        gasSensor->getDiagnosis(diag);
-        uint8_t regData[1];
-        gasSensor->getRegister(XENSIV_PAS_GAS_REG_DIAG, regData, 1);
-        TEST_ASSERT_EQUAL(regData[0], diag);
-    }
-
-    TEST_IFX(XENSIV_PAS_GAS, TestGetRegister)
-    {
-        uint8_t regData[1];
-        gasSensor->getRegister(testRegAddr, regData, 1);
-        uint8_t directRegData = 0;
-        gasSensor->getRegister(testRegAddr, &directRegData, 1);
-        TEST_ASSERT_EQUAL(regData[0], directRegData);
-    }
-
-    TEST_IFX(XENSIV_PAS_GAS, TestSetRegister)
-    {
-        uint8_t regData[1] = {0xAA};
-        gasSensor->setRegister(testRegAddr, regData, 1);
-        uint8_t regRead[1];
-        gasSensor->getRegister(testRegAddr, regRead, 1);
-        TEST_ASSERT_EQUAL(regData[0], regRead[0]);
-    }
-
-    // All tests below are HIL (hardware-in-the-loop)
-
-    TEST_IFX(XENSIV_PAS_GAS, TestEnd)
-    {
-        gasSensor->end();
-        uint8_t regData[1];
-        gasSensor->getRegister(XENSIV_PAS_GAS_REG_SENS_RST, regData, 1);
-    }
-
-    TEST_IFX(XENSIV_PAS_GAS, TestStartMeasure)
-    {
-        gasSensor->startMeasure(1, 0, nullptr, false);
-        uint8_t regDataH[1], regDataL[1];
-        gasSensor->getRegister(XENSIV_PAS_GAS_REG_MEAS_RATE_H, regDataH, 1);
-        gasSensor->getRegister(XENSIV_PAS_GAS_REG_MEAS_RATE_L, regDataL, 1);
-    }
-
-    TEST_IFX(XENSIV_PAS_GAS, TestStopMeasure)
-    {
-        gasSensor->stopMeasure();
-        uint8_t regData[1];
-        gasSensor->getRegister(XENSIV_PAS_GAS_REG_MEAS_CFG, regData, 1);
-    }
-
-    TEST_IFX(XENSIV_PAS_GAS, TestGetGasConcentrationUnitStr)
-    {
-        const char *unitStr = gasSensor->getGasConcentrationUnitStr();
-        TEST_ASSERT_NOT_NULL(unitStr);
-    }
-
-    TEST_IFX(XENSIV_PAS_GAS, TestGetPasGasErrorStr)
-    {
-        const char *errStr = gasSensor->getPasGasErrorStr(-1); // Example error code
-        TEST_ASSERT_NOT_NULL(errStr);
-    }
 }
 
-// Example HIL-style test for getProductID
+TEST_IFX(XENSIV_PAS_GAS, TestSetABOC)
+{
+    ABOC_t aboc = XENSIV_PAS_GAS_BOC_CFG_AUTOMATIC;
+    int16_t abocRef = 400;
+    Error_t err = gasSensor->setABOC(aboc, abocRef);
+    TEST_ASSERT_EQUAL(0, err);
+    uint8_t regData[2];
+    gasSensor->getRegister(XENSIV_PAS_GAS_REG_CALIB_REF_H, regData, 1);
+    uint16_t regValue = regData[0] << 8;
+    gasSensor->getRegister(XENSIV_PAS_GAS_REG_CALIB_REF_L, regData + 1, 1);
+    regValue |= regData[1];
+    TEST_ASSERT_EQUAL(abocRef, regValue);
+}
+
+TEST_IFX(XENSIV_PAS_GAS, TestForcedCompensationWorkflow)
+{
+    // Step 1: Perform forced compensation
+    uint16_t testValue = 400;
+    Error_t err = gasSensor->performForcedCompensation(testValue);
+    TEST_ASSERT_EQUAL(0, err);
+
+    // Step 2: Trigger three measurement cycles as required by forced compensation
+    for (int i = 0; i < 3; ++i)
+    {
+        gasSensor->startMeasure(10, 0, nullptr, false); // 10s period recommended
+        delay(1000);                                    // Wait for measurement
+        float dummy;
+        gasSensor->getGasConcentration(dummy);
+    }
+
+    // Step 3: Read the computed offset from the calibration register
+    uint8_t regData[2];
+    gasSensor->getRegister(XENSIV_PAS_GAS_REG_CALIB_REF_H, regData, 1);
+    uint16_t regValue = regData[0] << 8;
+    gasSensor->getRegister(XENSIV_PAS_GAS_REG_CALIB_REF_L, regData + 1, 1);
+    regValue |= regData[1];
+    TEST_ASSERT_TRUE(regValue != 0);
+
+    // Step 4: Clear forced compensation
+    err = gasSensor->clearForcedCompensation();
+    TEST_ASSERT_EQUAL(0, err);
+    gasSensor->getRegister(XENSIV_PAS_GAS_REG_CALIB_REF_H, regData, 1);
+    regValue = regData[0] << 8;
+    gasSensor->getRegister(XENSIV_PAS_GAS_REG_CALIB_REF_L, regData + 1, 1);
+    regValue |= regData[1];
+    TEST_ASSERT_EQUAL(0, regValue);
+}
+
+TEST_IFX(XENSIV_PAS_GAS, TestGetGasConcentration)
+{
+    float value = 0.0f;
+    gasSensor->getGasConcentration(value);
+    uint8_t regData[2];
+    gasSensor->getRegister(XENSIV_PAS_GAS_REG_GASCONC_H, regData, 1);
+    uint16_t regValue = regData[0] << 8;
+    gasSensor->getRegister(XENSIV_PAS_GAS_REG_GASCONC_L, regData + 1, 1);
+    regValue |= regData[1];
+    float regConc = (float)regValue;
+    TEST_ASSERT_EQUAL(regConc, value);
+}
+
+TEST_IFX(XENSIV_PAS_GAS, TestGetDiagnosis)
+{
+    Diag_t diag;
+    diag.u = 0;
+    Error_t err = gasSensor->getDiagnosis(diag);
+    TEST_ASSERT_EQUAL(0, err); // Ensure no error
+}
+
+TEST_IFX(XENSIV_PAS_GAS, TestGetRegister)
+{
+    uint8_t regData[1];
+    gasSensor->getRegister(testRegAddr, regData, 1);
+    uint8_t directRegData = 0;
+    gasSensor->getRegister(testRegAddr, &directRegData, 1);
+    TEST_ASSERT_EQUAL(regData[0], directRegData);
+}
+
+TEST_IFX(XENSIV_PAS_GAS, TestSetRegister)
+{
+    uint8_t regData[1] = {0x55};
+    gasSensor->setRegister(testRegAddr, regData, 1);
+    uint8_t regRead[1];
+    gasSensor->getRegister(testRegAddr, regRead, 1);
+    TEST_ASSERT_EQUAL(regData[0], regRead[0]);
+}
+
+TEST_IFX(XENSIV_PAS_GAS, TestStartMeasure)
+{
+    // Continuous mode (period = 10s)
+    Error_t err = gasSensor->startMeasure(10);
+    uint8_t regData[1];
+    TEST_ASSERT_EQUAL(0, err);
+    gasSensor->getRegister(XENSIV_PAS_GAS_REG_MEAS_CFG, regData, 1);
+    TEST_ASSERT_EQUAL(2, regData[0] & 0x03); // op_mode: CONTINUOUS
+
+    // Alarm mode (threshold = 500)
+    err = gasSensor->startMeasure(10, 500, nullptr, false);
+    TEST_ASSERT_EQUAL(0, err);
+    gasSensor->getRegister(XENSIV_PAS_GAS_REG_MEAS_CFG, regData, 1);
+    TEST_ASSERT_EQUAL(2, regData[0] & 0x03); // op_mode: CONTINUOUS (alarm is a feature of continuous)
+
+    // Check alarm threshold register
+    uint8_t alarmReg[2];
+    gasSensor->getRegister(XENSIV_PAS_GAS_REG_ALARM_TH_H, alarmReg, 1);
+    gasSensor->getRegister(XENSIV_PAS_GAS_REG_ALARM_TH_L, alarmReg + 1, 1);
+    uint16_t alarmValue = (alarmReg[0] << 8) | alarmReg[1];
+    TEST_ASSERT_EQUAL(500, alarmValue);
+}
+
+TEST_IFX(XENSIV_PAS_GAS, TestStopMeasure)
+{
+    gasSensor->stopMeasure();
+    uint8_t regData[1];
+    gasSensor->getRegister(XENSIV_PAS_GAS_REG_MEAS_CFG, regData, 1);
+    TEST_ASSERT_EQUAL(0, regData[0] & 0x03); // op_mode: IDLE
+}
+
+TEST_IFX(XENSIV_PAS_GAS, TestGetGasConcentrationUnitStr)
+{
+    const char *unitStr = gasSensor->getGasConcentrationUnitStr();
+    TEST_ASSERT_NOT_NULL(unitStr);
+}
+
+TEST_IFX(XENSIV_PAS_GAS, TestGetPasGasErrorStr)
+{
+    const char *errStr = gasSensor->getPasGasErrorStr(-1); // Example error code
+    TEST_ASSERT_NOT_NULL(errStr);
+}
+
 TEST_IFX(XENSIV_PAS_GAS, TestGetProductID)
 {
     uint8_t prodID = 0, revID = 0;
@@ -159,20 +217,30 @@ TEST_IFX(XENSIV_PAS_GAS, TestGetProductID)
 
 TEST_GROUP_RUNNER(XENSIV_PAS_GAS)
 {
-    RUN_TEST_CASE(XENSIV_PAS_GAS, TestNoHardwareInit);
     RUN_TEST_CASE(XENSIV_PAS_GAS, TestSetPressRef);
-    RUN_TEST_CASE(XENSIV_PAS_GAS, TestEnd);
-    RUN_TEST_CASE(XENSIV_PAS_GAS, TestStartMeasure);
-    RUN_TEST_CASE(XENSIV_PAS_GAS, TestStopMeasure);
-    RUN_TEST_CASE(XENSIV_PAS_GAS, TestGetDiagnosis);
-    RUN_TEST_CASE(XENSIV_PAS_GAS, TestSetABOC);
-    RUN_TEST_CASE(XENSIV_PAS_GAS, TestPerformForcedCompensation);
-    RUN_TEST_CASE(XENSIV_PAS_GAS, TestReset);
     RUN_TEST_CASE(XENSIV_PAS_GAS, TestGetProductID);
     RUN_TEST_CASE(XENSIV_PAS_GAS, TestGetRegister);
     RUN_TEST_CASE(XENSIV_PAS_GAS, TestSetRegister);
-    RUN_TEST_CASE(XENSIV_PAS_GAS, TestClearForcedCompensation);
+    RUN_TEST_CASE(XENSIV_PAS_GAS, TestStartMeasure);
+    RUN_TEST_CASE(XENSIV_PAS_GAS, TestStopMeasure);
+    RUN_TEST_CASE(XENSIV_PAS_GAS, TestGetDiagnosis);
     RUN_TEST_CASE(XENSIV_PAS_GAS, TestGetGasConcentration);
     RUN_TEST_CASE(XENSIV_PAS_GAS, TestGetGasConcentrationUnitStr);
     RUN_TEST_CASE(XENSIV_PAS_GAS, TestGetPasGasErrorStr);
+    RUN_TEST_CASE(XENSIV_PAS_GAS, TestSetABOC);
+    RUN_TEST_CASE(XENSIV_PAS_GAS, TestForcedCompensationWorkflow);
+}
+
+TEST_GROUP_RUNNER(XENSIV_PAS_GAS_CO2_I2C)
+{
+    currentSensorType = SENSOR_CO2;
+    currentInterfaceType = SENSOR_IF_I2C;
+    RUN_TEST_GROUP(XENSIV_PAS_GAS);
+}
+
+TEST_GROUP_RUNNER(XENSIV_PAS_GAS_CO2_UART)
+{
+    currentSensorType = SENSOR_CO2;
+    currentInterfaceType = SENSOR_IF_UART;
+    RUN_TEST_GROUP(XENSIV_PAS_GAS);
 }
--- tests/test_xensiv_pas_gas_r290.cpp
+++ tests/test_xensiv_pas_gas_r290.cpp
@@ -1,26 +1,47 @@
+
 #include "test_arduino_includes.hpp"
 #include "xensiv_pas_gas_r290_ino.hpp"
 
+extern GasType_t currentSensorType;
+extern SensorInterfaceType currentInterfaceType;
+
 TEST_GROUP(XENSIV_PAS_GAS_R290);
 static XENSIV_PAS_GASR290Ino *r290Sensor = nullptr;
-static Error_t r290Err;
-static uint8_t testRegAddr = XENSIV_PAS_GAS_REG_PROD_ID;
 
 TEST_SETUP(XENSIV_PAS_GAS_R290)
 {
-    r290Sensor = new XENSIV_PAS_GASR290Ino(&Wire);
-    Wire.begin();
+    if (currentInterfaceType == SENSOR_IF_I2C)
+    {
+        Wire.begin();
+        r290Sensor = new XENSIV_PAS_GASR290Ino(&Wire);
+    }
+    else
+    {
+        Serial.begin(9600);
+        r290Sensor = new XENSIV_PAS_GASR290Ino(&Serial);
+    }
+    Error_t err = r290Sensor->begin();
+    TEST_ASSERT_EQUAL(0, err);
 }
 TEST_TEAR_DOWN(XENSIV_PAS_GAS_R290)
 {
     delete r290Sensor;
     r290Sensor = nullptr;
+    if (currentInterfaceType == SENSOR_IF_I2C)
+    {
+        Wire.end();
+    }
+    else
+    {
+        Serial.end();
+    }
 }
 
 TEST_IFX(XENSIV_PAS_GAS_R290, TestGetDeviceID)
 {
     uint8_t devID = 0;
-    r290Sensor->getDeviceID(devID);
+    Error_t err = r290Sensor->getDeviceID(devID);
+    TEST_ASSERT_EQUAL(0, err);
     uint8_t regData[1];
     r290Sensor->getRegister(XENSIV_PAS_GAS_R290_REG_DEV_ID, regData, 1);
     uint8_t regDevID = (regData[0] & XENSIV_PAS_GAS_R290_REG_DEV_ID_VAL_MSK) >> XENSIV_PAS_GAS_R290_REG_DEV_ID_VAL_POS;
@@ -30,7 +51,8 @@ TEST_IFX(XENSIV_PAS_GAS_R290, TestGetDeviceID)
 TEST_IFX(XENSIV_PAS_GAS_R290, TestGetAlarmConfig)
 {
     bool activeHigh = false;
-    r290Sensor->getAlarmConfig(activeHigh);
+    Error_t err = r290Sensor->getAlarmConfig(activeHigh);
+    TEST_ASSERT_EQUAL(0, err);
     uint8_t regData[1];
     r290Sensor->getRegister(XENSIV_PAS_GAS_R290_REG_ALARM_CFG, regData, 1);
     bool regActiveHigh = (regData[0] & XENSIV_PAS_GAS_R290_REG_ALARM_CFG_ALARM_POL_MSK) >>
@@ -41,48 +63,46 @@ TEST_IFX(XENSIV_PAS_GAS_R290, TestGetAlarmConfig)
 TEST_IFX(XENSIV_PAS_GAS_R290, TestSetGetABOCCycle)
 {
     uint8_t daysSet = 7;
-    r290Sensor->setABOCCycle(daysSet);
+    Error_t err = r290Sensor->setABOCCycle(daysSet);
+    TEST_ASSERT_EQUAL(0, err);
     uint8_t regData[1];
     r290Sensor->getRegister(XENSIV_PAS_GAS_R290_REG_ABOC_CYCLE, regData, 1);
     uint8_t regDays = (regData[0] & XENSIV_PAS_GAS_R290_REG_ABOC_CYCLE_SMOOTHING_FACT_MSK) >>
                       XENSIV_PAS_GAS_R290_REG_ABOC_CYCLE_SMOOTHING_FACT_POS;
     TEST_ASSERT_EQUAL(daysSet, regDays);
     uint8_t daysGet = 0;
-    r290Sensor->getABOCCycle(daysGet);
+    err = r290Sensor->getABOCCycle(daysGet);
+    TEST_ASSERT_EQUAL(0, err);
     TEST_ASSERT_EQUAL(daysSet, daysGet);
 }
 
 TEST_IFX(XENSIV_PAS_GAS_R290, TestSetABOCPrefill)
 {
     uint8_t hours = 12;
-    r290Sensor->setABOCPrefill(hours);
-    uint8_t regData[1];
-    r290Sensor->getRegister(XENSIV_PAS_GAS_R290_REG_ABOC_PREFILL, regData, 1);
-    uint8_t regHours =
-        (regData[0] & XENSIV_PAS_GAS_R290_REG_ABOC_PREFILL_VAL_MSK) >> XENSIV_PAS_GAS_R290_REG_ABOC_PREFILL_VAL_POS;
-    TEST_ASSERT_EQUAL(hours, regHours);
+    Error_t err = r290Sensor->setABOCPrefill(hours);
+    TEST_ASSERT_EQUAL(0, err);
 }
 
 TEST_IFX(XENSIV_PAS_GAS_R290, TestSetGetDenoiseConfig)
 {
     uint8_t smoothingSet = 3;
-    r290Sensor->setDenoiseConfig(smoothingSet);
+    Error_t err = r290Sensor->setDenoiseConfig(smoothingSet);
+    TEST_ASSERT_EQUAL(0, err);
     uint8_t regData[1];
     r290Sensor->getRegister(XENSIV_PAS_GAS_R290_REG_DENOISE_CFG, regData, 1);
     uint8_t regSmoothing = (regData[0] & XENSIV_PAS_GAS_R290_REG_DENOISE_CFG_SMOOTHING_FACT_MSK) >>
                            XENSIV_PAS_GAS_R290_REG_DENOISE_CFG_SMOOTHING_FACT_POS;
     TEST_ASSERT_EQUAL(smoothingSet, regSmoothing);
     uint8_t smoothingGet = 0;
-    r290Sensor->getDenoiseConfig(smoothingGet);
+    err = r290Sensor->getDenoiseConfig(smoothingGet);
+    TEST_ASSERT_EQUAL(0, err);
     TEST_ASSERT_EQUAL(smoothingSet, smoothingGet);
 }
 
 TEST_IFX(XENSIV_PAS_GAS_R290, TestSelfTestErrorClear)
 {
-    r290Sensor->selfTestErrorClear(true);
-    uint8_t regData[1];
-    r290Sensor->getRegister(XENSIV_PAS_GAS_R290_REG_SELF_TEST_CLR, regData, 1);
-    // Optionally check specific bits if needed
+    Error_t err = r290Sensor->selfTestErrorClear(true);
+    TEST_ASSERT_EQUAL(0, err);
 }
 
 TEST_IFX(XENSIV_PAS_GAS_R290, TestGetSelfTestStatusString)
@@ -95,6 +115,8 @@ TEST_IFX(XENSIV_PAS_GAS_R290, TestGetSelfTestStatusString)
 
 TEST_GROUP_RUNNER(XENSIV_PAS_GAS_R290)
 {
+
+    RUN_TEST_GROUP(XENSIV_PAS_GAS);
     RUN_TEST_CASE(XENSIV_PAS_GAS_R290, TestGetDeviceID);
     RUN_TEST_CASE(XENSIV_PAS_GAS_R290, TestGetAlarmConfig);
     RUN_TEST_CASE(XENSIV_PAS_GAS_R290, TestSetGetABOCCycle);
@@ -102,4 +124,18 @@ TEST_GROUP_RUNNER(XENSIV_PAS_GAS_R290)
     RUN_TEST_CASE(XENSIV_PAS_GAS_R290, TestSetGetDenoiseConfig);
     RUN_TEST_CASE(XENSIV_PAS_GAS_R290, TestSelfTestErrorClear);
     RUN_TEST_CASE(XENSIV_PAS_GAS_R290, TestGetSelfTestStatusString);
+}
+
+TEST_GROUP_RUNNER(XENSIV_PAS_GAS_R290_I2C)
+{
+    currentSensorType = SENSOR_R290;
+    currentInterfaceType = SENSOR_IF_I2C;
+    RUN_TEST_GROUP(XENSIV_PAS_GAS_R290);
+}
+
+TEST_GROUP_RUNNER(XENSIV_PAS_GAS_R290_UART)
+{
+    currentSensorType = SENSOR_R290;
+    currentInterfaceType = SENSOR_IF_UART;
+    RUN_TEST_GROUP(XENSIV_PAS_GAS_R290);
 }
