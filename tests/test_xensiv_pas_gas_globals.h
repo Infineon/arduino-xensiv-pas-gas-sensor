c3aa1466baa5782437083d3a1de8ad5cb45eafd9
IFX-Anusha <Anusha.TR@infineon.com>
2025-11-20 22:21:34 +0530

    tests: Added Uart test variables.
    Signed-off-by: IFX-Anusha <Anusha.TR@infineon.com>

---
+++ tests/test_xensiv_pas_gas_globals.h
@@ -0,0 +1,13 @@
+#ifndef TEST_XENSIV_PAS_GAS_GLOBALS_H
+#define TEST_XENSIV_PAS_GAS_GLOBALS_H
+
+#include "xensiv_pas_gas_ino.hpp"
+
+// Interface type enum
+enum SensorInterfaceType { SENSOR_IF_I2C, SENSOR_IF_UART };
+
+// Global variables for test parameterization
+extern SensorInterfaceType currentInterfaceType;
+extern GasType_t currentSensorType;
+
+#endif // TEST_XENSIV_PAS_GAS_GLOBALS_H
